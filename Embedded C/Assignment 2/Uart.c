0aa323b1ccd0aaa4af167c8d6779e93d613d664a
Moo ALaa <moalaa@gmail.com>
2024-02-12 18:51:35 +0200

    correcting the fault

--- Embedded C/Assignment 2/Uart.c
+++ Embedded C/Assignment 2/Uart.c
@@ -1,5 +1,5 @@
 #include "Uart.h"
-#define UART0DR *((volatile unsigned int*)((unsigned int*)0x101f1000))
+#define UART0DR *((volatile unsigned int*)((unsigned int*) 0x101f1000))
 
 void uart_send_string(unsigned char *P_tx_string)
 {
